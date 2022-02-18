a280356a07f87179059fd52eb63b76dca62adfa0
vincentchen <vinscentchen@gmail.com>
2022-02-18 02:06:20 -0800

    tidying up chart 1 code

--- source/Chart_1/Data_Visualization_1.R
+++ source/Chart_1/Data_Visualization_1.R
@@ -2,17 +2,12 @@ library("ggplot2")
 library("dplyr")
 library("tidyr")
 library("plotly")
-getwd()
+
 sleep_data <- read.csv("../../data/sleepdata.csv")
-View(sleep_data)
- 
- getwd()
   
   sleep_depression_age <- sleep_data %>% 
-  select(id, AgeGroup, HADS_Depression, KSQ_SleepQualityIndex) 
+  select(AgeGroup, HADS_Depression, KSQ_SleepQualityIndex) 
   
-
-
 sleep_depression_age_plot <- ggplot(sleep_depression_age) +
   geom_col(mapping = aes(x = KSQ_SleepQualityIndex, y = HADS_Depression, fill = AgeGroup), position = "dodge") + 
   
