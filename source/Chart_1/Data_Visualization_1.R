24ca45b88d2b4b235d8561153ced5f7b6dd8fdc1
vincentchen <vinscentchen@gmail.com>
2022-02-18 16:06:01 -0800

    update chart 1

--- source/Chart_1/Data_Visualization_1.R
+++ source/Chart_1/Data_Visualization_1.R
@@ -4,21 +4,20 @@ library("tidyr")
 library("plotly")
 
 sleep_data <- read.csv("../../data/sleepdata.csv")
-  
-  sleep_depression_age <- sleep_data %>% 
-  select(AgeGroup, HADS_Depression, KSQ_SleepQualityIndex) 
-  
+
+sleep_depression_age <- sleep_data %>%
+  select(
+  AgeGroup, 
+  HADS_Depression, 
+  KSQ_SleepQualityIndex)
+
 sleep_depression_age_plot <- ggplot(sleep_depression_age) +
-  geom_col(mapping = aes(x = KSQ_SleepQualityIndex, y = HADS_Depression, fill = AgeGroup), position = "dodge") + 
-  
+  geom_col(mapping = aes(x = KSQ_SleepQualityIndex, y = HADS_Depression, fill = AgeGroup), position = "dodge") +
   labs(
-    title = "Sleep Quality versus Depression Levels Based on Age", 
+    title = "Sleep Quality versus Depression Levels Based on Age",
     x = "Sleep Quality of Individual",
     y = "Depression Level of Individual",
     color = "Urbanity"
   )
 
 ggplotly(sleep_depression_age_plot)
-
-
-
